28518469ae184757b681729a2babc9d7539d1e9b
veraborvinski <72395680+veraborvinski@users.noreply.github.com>
2024-01-05 16:37:47 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,23 @@
-resource "google_compute_instance_from_machine_image" "tpl" {
-  provider = google-beta
-  name     = var.machine_name
-  zone     = var.zone
+resource "google_compute_instance" "test-machine" {
+  name         = var.machine_name
+  machine_type = var.machine_type
+  zone         = var.zone
+  tags         = ["http-server","allow-http-ssh"]
 
-  source_machine_image = "projects/${var.project}/global/machineImages/videoserver"
+  boot_disk {
+    initialize_params {
+      image = var.image
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+
+    }
+  }
+
+  metadata = {
+    user-data = file("./setup.sh")
+  }
 }
